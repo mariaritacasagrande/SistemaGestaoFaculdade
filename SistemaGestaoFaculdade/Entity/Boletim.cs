c431b93e91c797c96b7e4a236cc1c950b1b3ffe3
Maria Rita Casagrande <mariarita@riseup.net>
2026-08-27 00:20:29 -0300

    Criado o boletim e a Matricula

---
+++ SistemaGestaoFaculdade/Entity/Boletim.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SistemaGestaoFaculdade.Entity
+{
+    public class Boletim
+    {
+
+        // Usei um dicionário aquipra associar cada disciplina a uma nota
+        public Dictionary<Disciplina, double> NotaPorDisciplina {  get; set; } = new Dictionary<Disciplina, double>();
+
+        // Regra de aprovação baseada no tipo de curso ( graduação aprova com no minimo 7 e pós com 8)
+        public string ObterSituacao(double nota, TipoCurso tipoCurso)
+        {
+            if (tipoCurso == TipoCurso.Graduação)
+            {
+                return nota >= 7.0 ? "Aprovado" : "Reprovado";
+            }
+            else
+            {
+                return nota >= 8.0 ? "Aprovado" : "Reprovado";
+            }
+        }
+    }
+}
---
+++ SistemaGestaoFaculdade/Entity/Matricula.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SistemaGestaoFaculdade.Entity
+{
+    public class Matricula
+    {
+
+        public Aluno Aluno { get; set; }    
+        public Curso Curso { get; set; }
+
+        //o boletim pertence a matricula, . Se o aluno cursa dois cursos ele tem dois boletins separados e independentes - esta nas regras
+
+        public Boletim Boletim { get; set; }
+
+        public Matricula(Aluno aluno, Curso curso)
+        {
+            Aluno = aluno;
+            Curso = curso;
+            Boletim = new Boletim(); // cria o boletim
+        }
+    }
+}
