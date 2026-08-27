3a99ac48b9bd66b3484897d5d577f09143a2bf23
Maria Rita Casagrande <mariarita@riseup.net>
2026-08-26 23:38:48 -0300

    Create Professor.cs

---
+++ SistemaGestaoFaculdade/Entity/Professor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace SistemaGestaoFaculdade.Entity
+{
+    // o professor é uma pessoa - então classe aluno base pessoa
+    public class Professor : Pessoa
+    {
+        // além do que foi herdado de pessoa, um professor tem Registro e Especialidade
+        public string Registro { get; set; }
+        public string Especialidade { get; set; }
+
+        //Construtor de Professor
+
+        public Professor(string nome, string cpf, string email, string registro, string especialidade) : base(nome, cpf, email)
+        {
+            Registro = registro;
+            Especialidade = especialidade;
+        }
+
+
+    }
+}
