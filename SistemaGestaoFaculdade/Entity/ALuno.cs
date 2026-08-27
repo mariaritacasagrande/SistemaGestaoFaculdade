b92b9d664b07903926d971a10e67b2b350d05c69
Maria Rita Casagrande <mariarita@riseup.net>
2026-08-26 23:31:38 -0300

    Create ALuno.cs
    obs - Criei o Aluno, mas ainda não criei a Matricula

---
+++ SistemaGestaoFaculdade/Entity/ALuno.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace SistemaGestaoFaculdade.Entity
+{
+    // o aluno é uma pessoa - então classe aluno base pessoa
+    public class Aluno : Pessoa
+    {
+    // além do que foi herdado de pessoa , um aluno também tem um numero de matricula
+    public string NumeroMatricula {  get; set; }
+        //um aluno pode estar matriculado em vários cursos por isso o List
+        public List<Matricula> Matriculas { get; set; } = new List<Matricula>(List);
+
+        //Construtor de Aluno
+
+        public Aluno(string nome, string cpf, string email,string numeroMatricula) : base (nome,cpf,email)
+        {
+            NumeroMatricula = numeroMatricula;
+        }
+
+
+    }
+}
